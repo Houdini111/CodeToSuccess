bcfd9327f05fecb34a6bbe558382f0f8b6ce7919
Jordan Hudgens <xela.notnef@gmail.com>
2017-06-21 15:34:45 +0000

    Integrated new gem into rails app for copyright

--- Gemfile
+++ Gemfile
@@ -59,3 +59,5 @@ gem 'devise', '~> 4.2'
 gem 'bootstrap', '~> 4.0.0.alpha6'
 
 gem 'jquery-rails'
+
+gem 'fenton_view_tool', git: 'https://github.com/Houdini111/fenton_view_tool'
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,8 @@ module ApplicationHelper
       content_tag(:p, greeting, class: "source-greeting")
     end
   end
+  
+  def copyright_generator
+    FentonViewTool::Renderer.copyright 'Alex Fenton', 'All rights reserved'
+  end
 end
