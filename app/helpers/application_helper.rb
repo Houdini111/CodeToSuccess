f39c4e5b5476ae9f664430239cf13e20b8446c32
Jordan Hudgens <xela.notnef@gmail.com>
2017-07-17 17:17:02 +0000

    Changed Tech News CSS

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -51,6 +51,10 @@ module ApplicationHelper
       {
         url: portfolios_path,
         title: "Portfolio"
+      },
+      {
+        url: tech_news_path,
+        title: "Tech News"
       }
     ]
   end
